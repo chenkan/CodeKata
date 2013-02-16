0f5b2cc5f1d8e6f30ba0eba7b151f7e8053be853
ChenKan <ckbsc@126.com>
2013-02-16 22:11:41 +0800

    使用方向键控制一条蛇游动

--- retro_snaker/game_screen.rb
+++ retro_snaker/game_screen.rb
@@ -2,55 +2,63 @@ require 'tk'
 
 def draw_brick_lines(dots_array, canvas)
 
+  bricks = []
+
   start_at = dots_array[0][:start_at]
           x =      dots_array.length - 1
   for i in 1..x do
     direction =            dots_array[i].keys[0]
     distance =              dots_array[i].values[0]
-    start_at = draw_brick_line(start_at, direction, distance, canvas)
+    tmp = draw_brick_line(start_at, direction, distance, canvas)
+    start_at = [tmp[0],tmp[1]]
+    bricks = bricks + tmp[2]
   end
+  bricks
 end
 
 def draw_brick_line(start_at, direction, distance, canvas)
   case direction
     when :right
-      to_right(start_at, distance, canvas)
-      return [start_at[0] + distance, start_at[1]]
+      return [start_at[0] + distance, start_at[1], to_right(start_at, distance, canvas)]
     when :down
-      to_down(start_at, distance, canvas)
-      return [start_at[0], start_at[1] + distance]
+      return [start_at[0], start_at[1] + distance, to_down(start_at, distance, canvas)]
     when :left
-      to_left(start_at, distance, canvas)
-      return [start_at[0] - distance, start_at[1]]
+      return [start_at[0] - distance, start_at[1], to_left(start_at, distance, canvas)]
     when :up
-      to_up(start_at, distance, canvas)
-      return [start_at[0], start_at[1] - distance]
-
+      return [start_at[0], start_at[1] - distance, to_up(start_at, distance, canvas)]
   end
 end
 
 def to_right(start_at, distance, canvas)
+  bricks = []
     for i in 0..distance
-      draw_brick(start_at[0] + i, start_at[1], canvas)
+      bricks.push draw_brick(start_at[0] + i, start_at[1], canvas)
     end
+  bricks
 end
 
 def to_down(start_at, distance, canvas)
+  bricks = []
   for i in 0..distance
-    draw_brick(start_at[0], start_at[1] + i, canvas)
+    bricks.push draw_brick(start_at[0], start_at[1] + i, canvas)
   end
+  bricks
 end
 
 def to_left(start_at, distance, canvas)
+  bricks = []
   for i in 0..distance
-    draw_brick(start_at[0] - i, start_at[1], canvas)
+    bricks.push draw_brick(start_at[0] - i, start_at[1], canvas)
   end
+  bricks
 end
 
 def to_up(start_at, distance, canvas)
+  bricks = []
   for i in 0..distance
-    draw_brick(start_at[0], start_at[1] - i, canvas)
+    bricks.push draw_brick(start_at[0], start_at[1] - i, canvas)
   end
+  bricks
 end
 
 def draw_brick(x, y, canvas)
@@ -62,43 +70,113 @@ def draw_brick(x, y, canvas)
   TkcRectangle.new(canvas,(x-1)*cell_width,(y-1)*cell_height,(x-1)*cell_width+brick_width,(y-1)*cell_height+brick_height).fill 'black'
 end
 
+def modify_snake(move_direction, snake)
+  if snake[1].keys[0] == :left || snake[1].keys[0] == :right
+
+  end
+
+  # 处理蛇的第一节
+  case move_direction
+    when :right
+      case snake[1].keys[0]
+        when :left
+          snake[1][:left] = snake[1].values[0] + 1
+        when :right
+          snake[1][:left] = snake[1].values[0] + 1
+        else
+          snake.insert 1, {:left => 1}
+      end
+      snake[0][:start_at] = [snake[0][:start_at][0] + 1, snake[0][:start_at][1]]
+    when :down
+      case snake[1].keys[0]
+        when :down
+          snake[1][snake[1].keys[0]] = snake[1].values[0] + 1
+        when :up
+          snake[1][snake[1].keys[0]] = snake[1].values[0] + 1
+        else
+          snake.insert 1, {:up => 1}
+      end
+      snake[0][:start_at] = [snake[0][:start_at][0], snake[0][:start_at][1] + 1]
+    when :left
+      case snake[1].keys[0]
+        when :left
+          snake[1][snake[1].keys[0]] = snake[1].values[0] + 1
+        when :right
+          snake[1][snake[1].keys[0]] = snake[1].values[0] + 1
+        else
+          snake.insert 1, {:right => 1}
+      end
+      snake[0][:start_at] = [snake[0][:start_at][0] - 1, snake[0][:start_at][1]]
+    when :up
+      case snake[1].keys[0]
+        when :down
+          snake[1][:down] = snake[1].values[0] + 1
+        when :up
+          snake[1][:down] = snake[1].values[0] + 1
+        else
+          snake.insert 1, {:down => 1}
+      end
+      snake[0][:start_at] = [snake[0][:start_at][0], snake[0][:start_at][1] - 1]
+  end
+
+  #if snake.length <= 2
+  #  return snake
+  #end
+
+  # 处理蛇的最后一节
+  x = snake.length - 1
+  if snake[x].values[0] == 1
+      snake = snake.reverse.drop(1).reverse
+  else
+    snake[x][snake[x].keys[0]] = snake[x].values[0] - 1
+  end
+  return snake
+end
+
 title_text='snake'
 root=TkRoot.new{title title_text}
 c=TkCanvas.new(root, :width => 640, :height => 640)
 
 c.pack
 #head
-
+# TODO 折线的描述方式很有问题
 draw_brick_lines([{:start_at => [2,2]},
                   {:right => 30},
-                  {:down => 30}, {:left => 30}, {:up => 30}
+                  {:down => 30},
+                  {:left => 30},
+                  {:up => 30}
                  ], c)
 
 
-x = 10
-y = 10
+snake = [{:start_at => [10,10]},
+         {:left => 9}]
 
-moving_brick = draw_brick x, y, c
+@snake_bricks = draw_brick_lines snake, c
+
 
 root.bind("Key-Right"){
-  moving_brick.delete() #TODO delete()以后，相应的对象是否会被自动回收，以避免内存溢出
-  x = x + 1
-  moving_brick = draw_brick x, y, c
+  @snake_bricks.each {|x|x.delete()} #TODO delete()以后，相应的对象是否会被自动回收，以避免内存溢出
+  snake = modify_snake :right, snake
+  puts snake
+  @snake_bricks = draw_brick_lines snake, c
 }
 root.bind("Key-Down"){
-  moving_brick.delete()
-  y = y + 1
-  moving_brick = draw_brick x, y, c
+  @snake_bricks.each {|x|x.delete()}
+  snake = modify_snake :down, snake
+  puts snake
+  @snake_bricks = draw_brick_lines snake, c
 }
 root.bind("Key-Left"){
-  moving_brick.delete()
-  x = x - 1
-  moving_brick = draw_brick x, y, c
+  @snake_bricks.each {|x|x.delete()}
+  snake = modify_snake :left, snake
+  puts snake
+  @snake_bricks = draw_brick_lines snake, c
 }
 root.bind("Key-Up"){
-  moving_brick.delete()
-  y = y - 1
-  moving_brick = draw_brick x, y, c
+  @snake_bricks.each {|x|x.delete()}
+  snake = modify_snake :up, snake
+  puts snake
+  @snake_bricks = draw_brick_lines snake, c
 }
 
 Tk.mainloop
