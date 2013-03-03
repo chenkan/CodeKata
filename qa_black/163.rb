0ee3a9edbc751bb4b0144868387341c4c2683a84
ChenKan <ckbsc@126.com>
2013-03-03 14:42:55 +0800

    网页小爬虫 - 迭代3 - 提取所有163子域名

--- qa_black/163.rb
+++ qa_black/163.rb
@@ -4,5 +4,6 @@ require 'iconv'
 conv = Iconv.new("UTF-8", "GBK")
 open('http://www.163.com') do |f|
   content = conv.iconv f.read
-  puts content
+  domains = content.scan(/\/\/(.{1,25}.163.com)/).uniq!.sort!    #TODO 正则表达式可以再优化
+  domains.each_index {|x| puts "#{x} - #{domains[x][0]}"}
 end
