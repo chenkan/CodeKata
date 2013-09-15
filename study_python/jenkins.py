ac561acacbeac8a9a6c3b23944e3e8df21d91e1b
ChenKan <ckbsc@126.com>
2013-09-15 22:55:27 +0800

    [dev] Query jenkins build status with python api

--- study_python/jenkins.py
+++ study_python/jenkins.py
@@ -1,5 +1,13 @@
-# ref link - https://github.com/salimfadhley/jenkinsapi/blob/master/jenkinsapi/jenkins.py
-# author ChenKan
+# Reference link
+# - http://pythonhosted.org/jenkinsapi/
+# - https://github.com/salimfadhley/jenkinsapi/blob/master/jenkinsapi/jenkins.py
+# - https://github.com/salimfadhley/jenkinsapi/blob/master/jenkinsapi/job.py
+# - https://github.com/salimfadhley/jenkinsapi/blob/master/jenkinsapi/build.py
+
+# Using curl to do the same work
+# - http://wiki.cloudbees.com/bin/view/DEV/Accessing+Jenkins+using+HTTP+Basic+Authentication
+
+# Author ChenKan
 
 from jenkinsapi.jenkins import Jenkins
 
@@ -10,8 +18,14 @@ PASS_WORD   = 'admin'
 
 def trigger_build(J):
     J.build_job(JOB_NAME)
-    return 'have built <' + JOB_NAME + '>'
+    print 'have built <' + JOB_NAME + '>'
+
+def query_latest_build(J):
+    job = J[JOB_NAME]
+    build = job.get_last_build()
+    print "#%d - %s" %(build.get_number(), build.get_status())
 
 if __name__ == '__main__':
     J = Jenkins(JENKINS_URL, USER_NAME, PASS_WORD)
-    print trigger_build(J)
+    # trigger_build(J)
+    query_latest_build(J)
